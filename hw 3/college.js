7d4c6d33433f87052c442004be26ac3dd3627de8
Sreedev10 <sreedevskurup10@gmail.com>
2022-07-01 09:09:26 -0700

    completed

---
+++ hw 3/college.js
@@ -0,0 +1,35 @@
+loginBtnEvent=()=>{
+    Name=(document.getElementById("Name").value)
+    DOB=(document.getElementById("DOB").value)
+    REGno=(document.getElementById("REGno").value)
+
+    console.log(Name)
+    alert(Name)
+
+    console.log(DOB)
+    alert(DOB)
+
+    console.log(REGno)
+    alert(REGno)
+
+}
+
+registerBtnEvent=()=>{
+     
+    name=(document.getElementById("name").value)
+    batch=(document.getElementById("batch").value)
+    address=(document.getElementById("address").value)
+    
+    console.log(name)
+    alert(name)
+
+    console.log(batch)
+    alert(batch)
+
+    console.log(address)
+    alert(address)
+
+   
+}
+
+
---
+++ hw 4/food.js
@@ -0,0 +1,22 @@
+orderBtnEvent=()=>{
+    Name=(document.getElementById("Name").value)
+    address=(document.getElementById("address").value)
+    pincode=(document.getElementById("pincode").value)
+    phoneno=(document.getElementById("phoneno").value)
+    email=(document.getElementById("email").value)
+
+    console.log(Name)
+    alert(Name)
+
+    console.log(address)
+    alert(address)
+
+    console.log(pincode)
+    alert(pincode)
+    
+    console.log(phoneno)
+    alert(phoneno)
+
+    console.log(email)
+    alert(email)
+}
