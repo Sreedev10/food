bcc0817c21130e8af7e8a63b906406d26b32ecaf
Sreedev10 <sreedevskurup10@gmail.com>
2022-07-15 19:39:30 -0700

    completed

---
+++ hw 5/movie.js
@@ -0,0 +1,35 @@
+bookBtnEvent=()=>{
+    Name=(document.getElementById("Name").value)
+    phoneno=(document.getElementById("phoneno").value)
+    email=(document.getElementById("email").value)
+    moviename=(document.getElementById("movie name").value)
+    numberofticket=(document.getElementById("number of ticket").value)
+    date=(document.getElementById("date").value)
+    time=(document.getElementById("time").value)
+    city=(document.getElementById("city").value)
+
+
+    console.log(Name)
+    alert(Name)
+
+    console.log(phoneno)
+    alert(phoneno)
+
+    console.log(email)
+    alert(email)
+    
+    console.log(moviename)
+    alert(moviename)
+
+    console.log(numberofticket)
+    alert(numberofticket)
+
+    console.log(date)
+    alert(date)
+
+    console.log(time)
+    alert(time)
+    
+    console.log(city )
+    alert( city)
+}
